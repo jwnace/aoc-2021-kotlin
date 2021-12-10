8f519c7de4fc7123046fb1015080d4c43c854418
Joe Nace <jwnace@gmail.com>
2021-12-10 11:15:56 -0500

    cleaned up solution for day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -33,53 +33,35 @@ fun main() {
     fun part2() {
         val map = mapOf<Char, Char>('(' to ')', '[' to ']', '{' to '}', '<' to '>')
         val points = mapOf<Char, Int>('(' to 1, '[' to 2, '{' to 3, '<' to 4)
-        val linesToRemove = mutableListOf<Int>()
-
-        for (i in input.indices) {
-            val line = input[i]
-            val stack = Stack<Char>()
-
-            for (j in line.indices) {
-                if (map.containsKey(line[j])) {
-                    stack.push(line[j])
-                } else {
-                    val temp = stack.pop()
-
-                    if (map[temp] != line[j]) {
-                        linesToRemove.add(i)
-                    }
-                }
-            }
-        }
-
-        val lines = input.toMutableList()
-
-        for (i in linesToRemove.sortedDescending()) {
-            lines.removeAt(i)
-        }
-
         val scores = mutableListOf<Long>()
 
-        for (line in lines) {
+        for (line in input) {
             val stack = Stack<Char>()
+            var corrupted = false
             var score = 0L
 
             for (i in line.indices) {
                 if (map.containsKey(line[i])) {
                     stack.push(line[i])
                 } else {
-                    stack.pop()
+                    val temp = stack.pop()
+
+                    if (map[temp] != line[i]) {
+                        corrupted = true
+                    }
                 }
             }
 
-            while (stack.size > 0) {
-                val temp = stack.pop()
+            if (!corrupted) {
+                while (stack.size > 0) {
+                    val temp = stack.pop()
 
-                score *= 5
-                score += points[temp] ?: 0
+                    score *= 5
+                    score += points[temp] ?: 0
+                }
+
+                scores.add(score)
             }
-
-            scores.add(score)
         }
 
         val answer = scores.sorted()[scores.size / 2]
